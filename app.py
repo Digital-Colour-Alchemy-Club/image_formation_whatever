2196ec5e3629d3f534551ec743dc85d453182a7e
Zach Lewis <zachcanbereached@gmail.com>
2020-12-28 20:47:02 -0500

    Improve caching in lookup_method_tests

--- app.py
+++ app.py
@@ -142,19 +142,20 @@ def lookup_method_tests():
         value=0.0,
         step=0.1)
 
+    @st.cache
     def apply_inverse_EOTF(RGB):
         return RGB**(1.0 / EOTF)
 
-    # @st.cache
+    @st.cache
     def video_buffer(x):
         return ((2.0**exposure) * x)
 
-    # @st.cache
+    @st.cache
     @maxrgb_lookup
     def video_buffer_maxrgb(x):
         return video_buffer(x)
 
-    # @st.cache
+    @st.cache
     @norm_lookup(degree=5, weights=[1.22, 1.20, 0.58])
     def video_buffer_norm(x):
         return video_buffer(x)
@@ -165,17 +166,24 @@ def lookup_method_tests():
         img = colour.read_image(img_path)[..., 0:3]
         return img
 
+    @st.cache
+    def compare_methods():
+        img = get_marcie()
+        per_channel = video_buffer(img)
+        maxrgb = video_buffer_maxrgb(img)
+        norm = video_buffer_norm(img)
+        return [apply_inverse_EOTF(i) for i in [per_channel, maxrgb, norm]]
+
+    per_channel, maxrgb, norm = compare_methods()
+
     st.subheader('Per-channel')
-    img = apply_inverse_EOTF(video_buffer(get_marcie()))
-    st.image(img, clamp=[0., 1.], use_column_width=True)
+    st.image(per_channel, clamp=[0., 1.], use_column_width=True)
 
     st.subheader('maxRGB')
-    img = apply_inverse_EOTF(video_buffer_maxrgb(get_marcie()))
-    st.image(img, clamp=[0., 1.], use_column_width=True)
+    st.image(maxrgb, clamp=[0., 1.], use_column_width=True)
 
     st.subheader('Yellow-Weighted Norm')
-    img = apply_inverse_EOTF(video_buffer_norm(get_marcie()))
-    st.image(img, clamp=[0., 1.], use_column_width=True)
+    st.image(norm, clamp=[0., 1.], use_column_width=True)
 
 
 def about():
