5ac51c48f1fa4a767ab4168216aaaf8011ddfa1f
Zach Lewis <zachcanbereached@gmail.com>
2021-01-05 19:53:50 -0500

    Fix OIIO / stdlib conflict
    Update conda.txt to install py-openimageio
    
    
    Print OIIO version in diagnostics
    
    
    Migrate requirements from apt-get to conda
    
    
    Adjust libgcc version
    
    
    Update libgcc once more
    
    
    Remove reference to specific openimageio conda hash
    
    
    Add py-openimageio to conda.txt
    
    
    Adjust conda reqs
    
    
    Clean up diagnostics
    
    
    Fix conda_channels.txt filename

--- apps/diagnostics.py
+++ apps/diagnostics.py
@@ -10,22 +10,31 @@ from helpers import st_stdout
 
 def diagnostics(local_data="."):
     st.header("Streamlit instance info")
-    st.write(get_profile())
-    st.header("Python ")
-    st.subheader("System paths")
-    st.write(sys.path)
-    st.subheader("`colour-science` library info")
-    with st_stdout("code"):
-        colour.utilities.describe_environment()
     st.subheader("Locally-installed libraries")
     try:
         import PyOpenColorIO as ocio
 
-        st.write(f"PyOpenColorIO: v{ocio.__version__}")
+        st.write(f"PyOpenColorIO v{ocio.__version__}")
     except ImportError:
         pass
+    try:
+        import OpenImageIO as oiio
 
-    st.header("Local contents")
-    st.write(local_data)
+        st.write(f"OpenImageIO v{oiio.__version__}")
+    except:
+        pass
+
+    st.subheader("`colour-science` library info")
     with st_stdout("code"):
+        colour.utilities.describe_environment()
+
+    st.subheader("System")
+    st.write(get_profile())
+
+    st.subheader("Local contents")
+    with st_stdout("code"):
+        print(str(local_data))
         fs.open_fs(str(local_data)).tree()
+
+    st.subheader("Python system paths")
+    st.write(sys.path)
