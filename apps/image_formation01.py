ab474b970e8408ad9c89db44acd035a293a279d4
sobotka <troy.sobotka@gmail.com>
2021-04-17 12:26:59 -0700

    Improve difference view
    Improve the difference view by estimating the actual luminance
    of overage. It's a gamut volume mapping inception loop!

--- apps/image_formation01.py
+++ apps/image_formation01.py
@@ -264,13 +264,16 @@ class generic_aesthetic_transfer_function(AbstractLUTSequenceOperator):
         # Try to give an idea of how much out of whack the values are by
         # adding achromatic light to the difference view based on how
         # much the difference exceeds the maximum output at the display.
-        diff_diff = (
-            RGB_luminance_target_diff[..., RGB_luminance_target_diff > 1.0] - 1.0
-        ).reshape((-1, 1))
+        additional = (
+            RGB_luminance_target_diff[np.any(RGB_luminance_target_diff > 1.0, axis=-1)]
+            - 1.0
+        )
+        additional[additional < 0.0] = 0.0
+        additional_luminance = self.derive_luminance(additional).reshape((-1, 1))
 
         RGB_luminance_target_diff[
             np.any(RGB_luminance_target_diff > 1.0, axis=-1)
-        ] += diff_diff
+        ] += additional_luminance
 
         output_RGBs = RGB_luminance_target_diff
 
